fd292f1aa18a5e193b1120d1407f3606eb7760c9
FelipeTinel <felipemacedotinel@gmail.com>
2026-06-04 07:24:06 -0300

    feat: add type and gender enums

---
+++ include/gender.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+
+enum class Gender {
+
+    ACTION,
+    COMEDY,
+    ROMANCE,
+    HORROR,
+    SUSPENSE,
+    DRAMA,
+    SCIENCE_FICTION,
+
+};
---
+++ include/type.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <iostream>
+
+enum class Type {
+
+    MOVIE,
+    SERIES,
+    DOCUMENTARY,
+    ANIME,
+    CARTOON,
+
+};
