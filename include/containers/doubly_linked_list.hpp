1f133939958b00000e89a1b0364007377930c048
FelipeTinel <felipemacedotinel@gmail.com>
2026-07-10 09:10:22 -0300

    refactor: remove doublylinked list cpp

--- include/containers/doubly_linked_list.hpp
+++ include/containers/doubly_linked_list.hpp
@@ -23,3 +23,54 @@ class DoublyLinkedList {
 
 
 };
+
+
+template <typename T>
+void DoublyLinkedList<T>::insert(const T &info) {
+
+    Node<T> * new_node = new Node(info);
+    new_node->previous = nullptr;
+
+    if (head == nullptr) tail = new_node;
+    else head->previous = new_node;
+    new_node->next = head;
+    head = new_node;
+    current = head;
+
+}
+
+template <typename T>
+T DoublyLinkedList<T>::pop (const T &info) {
+
+    Node<T> * temp = tail;
+
+    while (temp != nullptr) {
+
+        if (content.get_id() == temp->content.get_id()) {
+
+            if (temp->previous != nullptr) temp->previous->next = temp->next;
+            else head = temp->next;
+
+            if (temp->next != nullptr) temp->next->previous = temp->previous;
+            else tail = temp->previous;
+
+            Content save_content = temp->content;
+            
+            if (current == temp) current = head;
+
+            if (current == temp) {
+            current = temp->next != nullptr ? temp->next : temp->previous;
+}
+
+            delete temp;
+            return save_content;
+
+        }
+
+        temp = temp->previous;
+
+    }
+
+    return content;
+
+}
