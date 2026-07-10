4f751ce7a8d2e150afe2a9a3f768bb92988db480
FelipeTinel <felipemacedotinel@gmail.com>
2026-07-10 10:03:50 -0300

    feat: DoublyLinkedList finalized

--- include/containers/doubly_linked_list.hpp
+++ include/containers/doubly_linked_list.hpp
@@ -1,25 +1,25 @@
 #include <iostream>
 
 #include "node.hpp"
-#include "content.hpp"
 
 template <typename T>
 class DoublyLinkedList {
 
-    Node * head, * tail, * current;
+    Node<T> * head, * tail;
 
     public:
 
-        DoublyLinkedList(): head(nullptr) {}
-        DoublyLinkedList(Node * head): head(head) {}
+        DoublyLinkedList(): head(nullptr), tail(nullptr) {}
+        DoublyLinkedList(Node<T> * head): head(head) {}
     
         ~ DoublyLinkedList ();
     
         void insert (const T &info);
-        T pop (const T &info);
+        T pop (int id);
+        T * search (int id) const;
 
         Node<T> * get_head() const { return head; }
-        Node<T> * get_tail() const; { return tail; }
+        Node<T> * get_tail() const { return tail; }
 
 
 };
@@ -42,13 +42,13 @@ void DoublyLinkedList<T>::insert(const T &info) {
 }
 
 template <typename T>
-T DoublyLinkedList<T>::pop (const T &info) {
+T DoublyLinkedList<T>::pop (int id) {
 
     Node<T> * temp = tail;
 
     while (temp != nullptr) {
 
-        if (info.get_id() == temp->info.get_id()) {
+        if (temp->info.get_id() == id) {
 
             if (temp->previous != nullptr) temp->previous->next = temp->next;
             else head = temp->next;
@@ -66,6 +66,38 @@ T DoublyLinkedList<T>::pop (const T &info) {
 
     }
 
-    return info;
+    return T();
 
+}
+
+template <typename T>
+DoublyLinkedList<T>::~DoublyLinkedList() {
+
+    Node<T>* actual = head;
+    Node<T>* ahead = nullptr;
+
+    while (actual != nullptr) {
+
+        ahead = actual->next;
+        
+        delete actual;
+
+        actual = ahead;
+    }
+
+    head = nullptr;
+    tail = nullptr;
+}
+
+template <typename T>
+T * DoublyLinkedList<T>::search(int id) const{
+    
+    Node<T>* node = head;
+    
+    while (node != nullptr) {
+        if (node->info.get_id() == id) return &node->info;
+        node = node->next;
+    }
+    
+    return nullptr;
 }
